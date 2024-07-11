db9ba1a9edb41cd16b07375f431d5534da454d67
Amit Singh <47208399+aamitsinghh10@users.noreply.github.com>
2024-07-11 13:55:16 +0530

    Add files via upload

---
+++ springsecuritybasic/springsecuritybasic/src/main/java/com/security/springsecuritybasic/Controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.security.springsecuritybasic.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "Welcome to Spring boot Security Series";
+    }
+}
