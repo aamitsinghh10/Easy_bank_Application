93da81c917aa9213b4afc92631520018e7468e8f
Amit Singh <47208399+aamitsinghh10@users.noreply.github.com>
2024-07-12 17:57:20 +0530

    Add files via upload

--- springSecurityEasyBanking/EasyBanking/src/main/java/com/security/easybanking/Controller/LoginController.java
+++ springSecurityEasyBanking/EasyBanking/src/main/java/com/security/easybanking/Controller/LoginController.java
@@ -5,6 +5,7 @@ import com.security.easybanking.Repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +15,15 @@ public class LoginController {
 
     @Autowired
     CustomerRepository customerRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody Customer customer){
         Customer savedCustomer = null;
         ResponseEntity response = null;
         try{
+            String hashPwd = passwordEncoder.encode(customer.getPwd());
+            customer.setPwd(hashPwd);
             savedCustomer = customerRepository.save(customer);
             if(savedCustomer.getId()>0){
                 response = ResponseEntity.status(HttpStatus.CREATED).body("Given user details are successfully Registered");
